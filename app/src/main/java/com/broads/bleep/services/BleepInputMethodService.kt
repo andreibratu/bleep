b074bb8bbec5da117ebe8f17b1fad0fb2c6e9656
Adrian Popa <adrian.popa@redabacus.eu>
2019-05-18 01:50:35 +0300

    Added basic keyboard layout

---
+++ app/src/main/java/com/broads/bleep/services/BleepInputMethodService.kt
@@ -0,0 +1,114 @@
+package com.broads.bleep.services
+
+import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.Keyboard
+import android.inputmethodservice.KeyboardView
+
+import android.view.View
+import android.view.KeyEvent
+import android.view.KeyEvent.KEYCODE_ENTER
+
+import android.text.TextUtils
+
+import android.media.AudioManager
+import android.os.Vibrator
+
+import android.content.Context
+import android.os.VibrationEffect
+import android.os.VibrationEffect.DEFAULT_AMPLITUDE
+
+import com.broads.bleep.R
+
+class BleepInputMethodService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
+
+    private var keyboardView: KeyboardView? = null
+    private var keyboard: Keyboard? = null
+
+    private var caps = false
+
+    private var am: AudioManager? = null
+    private var v: Vibrator? = null
+
+    override fun onCreateInputView(): KeyboardView? {
+        keyboardView = View.inflate(this, R.layout.keyboard_view, null) as KeyboardView
+        keyboard = Keyboard(this, R.xml.keys_layout)
+        keyboardView?.keyboard = keyboard
+        keyboardView?.setOnKeyboardActionListener(this)
+        return keyboardView
+    }
+
+    override fun onPress(i: Int) {
+
+    }
+
+    override fun onRelease(i: Int) {
+
+    }
+
+    override fun onKey(primaryCode: Int, keyCodes: IntArray) {
+        playSound(primaryCode)
+        val inputConnection = currentInputConnection
+        if (inputConnection != null) {
+            when (primaryCode) {
+                Keyboard.KEYCODE_DELETE -> {
+                    val selectedText = inputConnection.getSelectedText(0)
+
+                    if (TextUtils.isEmpty(selectedText)) {
+                        inputConnection.deleteSurroundingText(1, 0)
+                    } else {
+                        inputConnection.commitText("", 1)
+                    }
+                }
+                Keyboard.KEYCODE_SHIFT -> {
+                    caps = !caps
+                    keyboard?.isShifted = caps
+                    keyboardView?.invalidateAllKeys()
+                }
+                Keyboard.KEYCODE_DONE -> inputConnection.sendKeyEvent(
+                    KeyEvent(
+                        KeyEvent.ACTION_DOWN,
+                        KEYCODE_ENTER
+                    )
+                )
+                else -> {
+                    var code = primaryCode.toChar()
+                    if (Character.isLetter(code) && caps) {
+                        code = Character.toUpperCase(code)
+                    }
+                    inputConnection.commitText(code.toString(), 1)
+                }
+            }
+        }
+    }
+
+    override fun onText(charSequence: CharSequence) {
+
+    }
+
+    override fun swipeLeft() {
+
+    }
+
+    override fun swipeRight() {
+
+    }
+
+    override fun swipeDown() {
+
+    }
+
+    override fun swipeUp() {
+
+    }
+
+    private fun playSound(keyCode: Int) {
+        v?.vibrate(VibrationEffect.createOneShot(50, DEFAULT_AMPLITUDE))
+        am = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        when (keyCode) {
+            32 -> am?.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR)
+            Keyboard.KEYCODE_DONE, 10 -> am?.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN)
+            Keyboard.KEYCODE_DELETE -> am?.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE)
+            else -> am?.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD)
+        }
+    }
+}
