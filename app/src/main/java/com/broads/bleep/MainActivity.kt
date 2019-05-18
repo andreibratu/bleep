a8a6e36dd8c337f1b2c9eab693436716b48a4fe2
SabaAlex <sabadusalex@gmail.com>
2019-05-18 15:10:40 +0300

    Inca nu mere

--- app/src/main/java/com/broads/bleep/MainActivity.kt
+++ app/src/main/java/com/broads/bleep/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.broads.bleep
 
-import android.support.v7.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.support.v13.view.inputmethod.EditorInfoCompat
+import android.support.v13.view.inputmethod.InputConnectionCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputConnection
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +15,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+/*
+    private val editText = object : EditText(this) {
+        override fun onCreateInputConnection(editorInfo: EditorInfo): InputConnection {
+
+            val ic: InputConnection = super.onCreateInputConnection(editorInfo)
+            EditorInfoCompat.setContentMimeTypes(editorInfo, arrayOf("audio/mp4"))
+
+            val callback = InputConnectionCompat.OnCommitContentListener { inputContentInfo, flags, _ ->
+
+                val lacksPermission = (flags and InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0
+                // read and display inputContentInfo asynchronously
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1 && lacksPermission) {
+                    try {
+                        inputContentInfo.requestPermission()
+                    } catch (e: Exception) {
+                        return@OnCommitContentListener false
+                    }
+
+                }
+
+                // read and display inputContentInfo asynchronously.
+                // call inputContentInfo.releasePermission() as needed.
+
+                true
+            }
+            return InputConnectionCompat.createWrapper(ic, editorInfo, callback)
+        }
+    }*/
 }
--- app/src/main/java/com/broads/bleep/services/BleepInputMethodService.kt
+++ app/src/main/java/com/broads/bleep/services/BleepInputMethodService.kt
@@ -14,13 +14,63 @@ import android.media.AudioManager
 import android.os.Vibrator
 
 import android.content.Context
+import android.os.Build
 import android.os.VibrationEffect
 import android.os.VibrationEffect.DEFAULT_AMPLITUDE
-import android.widget.Toast
+import android.support.v13.view.inputmethod.EditorInfoCompat
+import android.support.v13.view.inputmethod.InputConnectionCompat
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputConnection
+import android.widget.EditText
 
+import android.content.ClipDescription
+import android.net.Uri
+
+import android.support.v13.view.inputmethod.InputContentInfoCompat
+import android.view.inputmethod.InputConnection.INPUT_CONTENT_GRANT_READ_URI_PERMISSION
+import android.view.inputmethod.InputContentInfo
 import com.broads.bleep.R
+import java.io.File
+
 
 class BleepInputMethodService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
+    
+
+/*
+    private val editText = object : EditText(this) {
+        override fun onCreateInputConnection(editorInfo: EditorInfo): InputConnection {
+            val ic: InputConnection = super.onCreateInputConnection(editorInfo)
+            EditorInfoCompat.setContentMimeTypes(editorInfo, arrayOf("audio/mp4"))
+
+            val callback = InputConnectionCompat.OnCommitContentListener { inputContentInfo, flags, _ ->
+
+                val lacksPermission = (flags and InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0
+                // read and display inputContentInfo asynchronously
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1 && lacksPermission) {
+                    try {
+                        inputContentInfo.requestPermission()
+                    } catch (e: Exception) {
+                        return@OnCommitContentListener false
+                    }
+                }
+                true
+            }
+            return InputConnectionCompat.createWrapper(ic, editorInfo, callback)
+        }
+    }*/
+/*
+    fun commitM4PFile(contentUri: Uri, imageDescription: String) {
+        val inputContentInfo = InputContentInfoCompat(contentUri,
+            ClipDescription(imageDescription, arrayOf("audio/mp4")), null)
+        val inputConnection = currentInputConnection
+        val editorInfo = currentInputEditorInfo
+        var flags = 0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            flags = flags or InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION
+        }
+        InputConnectionCompat.commitContent(inputConnection, editorInfo, inputContentInfo, flags, null)
+    }*/
+
 
     private var keyboardView: KeyboardView? = null
     private var keyboard: Keyboard? = null
@@ -76,8 +126,12 @@ class BleepInputMethodService : InputMethodService(), KeyboardView.OnKeyboardAct
                     )
                 )
                 KEYCODE_SEND_BLEEP -> {
-                    Toast.makeText(this, "Bleep sent", Toast.LENGTH_SHORT).show()
-                    // TODO: Add service call for sending a bleep.
+
+                    val myUri = Uri.fromFile(File("/app/src/main/res/test.mp4"))
+                    val clipDescription = ClipDescription("anthem", arrayOf("audio/mp4"))
+                    val contentInfo = InputContentInfo(myUri, clipDescription)
+                    inputConnection.commitContent(contentInfo, INPUT_CONTENT_GRANT_READ_URI_PERMISSION, null)
+                    ///commitM4PFile(myUri, "Rusian anthem")
                 }
                 else -> {
                     var code = primaryCode.toChar()
