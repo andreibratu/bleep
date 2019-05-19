ff56977f81f5c6fcffad089f28083308842f32b6
Andrei Bratu <bratu.andrei@outlook.co>
2019-05-19 09:17:24 +0300

    Split into db calls and shorten api call

--- app/src/main/java/com/broads/bleep/entities/Bleep.kt
+++ app/src/main/java/com/broads/bleep/entities/Bleep.kt
@@ -2,5 +2,4 @@ package com.broads.bleep.entities
 
 import java.io.File
 
-data class Bleep(val title: String, val publisher: String = "", val popularity: Int = 0,
-                 val bleepRef: String = "", var bleep: File? = null, var url: String = "")
+data class Bleep(val title: String, var url: String? = "")
